d61928dd5daec5ea700578a6e2bcc28defe39f45
wjjxqx <37838883+wjjxqx@users.noreply.github.com>
2023-11-26 15:58:38 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -19,7 +19,24 @@ rm -rf feeds/luci/applications/luci-app-netdata
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server package/luci-app-ssr-mudb-server
+svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-eqos package/luci-app-eqos
+svn other luci-app-phtunnel kiddin9/openwrt-packages/trunk
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-phtunnel package/luci-app-phtunnel
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-weburl package/luci-app-control-weburl
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-timecontro package/luci-app-timecontro
+svn export https://github.com/kiddin9/openwrt-packages/trunk/ngrokc package/ngrokc
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ngrokc package/luci-app-ngrokc
+svn export https://github.com/kiddin9/openwrt-packages/trunk/phtunnel package/phtunnel
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-webd package/luci-app-webd
+svn export https://github.com/kiddin9/openwrt-packages/trunk/webd package/webd
 # svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite package/msd_lite
 
 # 科学上网插件
